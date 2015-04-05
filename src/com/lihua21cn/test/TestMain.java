0576a8d39e98fae232d9dedb993dd5d059fcc1f8
lihua21cn <lihua21cn@163.com>
2015-04-05 10:43:23 +0800

    create first git test program

---
+++ src/com/lihua21cn/test/TestMain.java
@@ -0,0 +1,13 @@
+package com.lihua21cn.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		if(null != args && args.length > 0){
+			System.out.println(args[0]);
+		}
+		
+		System.out.println("=== end ===");
+	}
+
+}
